0ae35981cd15ea2c9a420b71644ba7d17bb88cf0
Alexandr <a.melentiev@inbox.ru>
2022-03-21 13:21:34 +0300

    commit2

--- plan.ru/ocpanel/page/tit1.php
+++
@@ -1,5 +0,0 @@
-<?
-session_start();
-require_once '../mod1123312mod.php';
-?>
-метрика
--- plan.ru/ocpanel/page/tit4/delmod.php
+++
@@ -1,5 +0,0 @@
-<?
-session_start();
-require_once '../../mod1123312mod.php';
-require_once '../../../connect.php';
-$sql = mysqli_query($dbc, 'DELETE FROM `mail` WHERE ID='.(int)$_POST['usosid']);
